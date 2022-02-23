02432dd62dc34097d62278580236510a7e30548f
Lorenz Steiner <lorenz.steiner@student.htldornbirn.at>
2022-02-23 20:04:43 +0100

    Functioning Base

--- src/app/json-modifier/json-modifier.component.ts
+++ src/app/json-modifier/json-modifier.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit, Output} from '@angular/core';
 import {Playlist} from "../playlist";
 import Sortable, { MultiDrag, Swap } from 'sortablejs';
 import {animate} from "@angular/animations";
+import {colors} from "@angular/cli/utilities/color";
 
 @Component({
   selector: 'app-json-modifier',
@@ -11,6 +12,10 @@ import {animate} from "@angular/animations";
 export class JsonModifierComponent implements OnInit {
 
   playlist: Playlist = new Playlist();
+  arr: string[] = [];
+
+  notAssignable: string[] = ["\u00fc", "\u00e4", "\u00f6", " ", "/", "\u00df", "\u00dc"];
+  nameCorrect: string[] = new Array(this.arr.length);
   sortable?:Sortable;
 
   constructor() {
@@ -23,15 +28,25 @@ export class JsonModifierComponent implements OnInit {
 
   onFileUpload(event: Event){
 
+
     // @ts-ignore
     let file:File = event.target.files[0];
-
     if(file){
       const fileReader = new FileReader();
       fileReader.readAsText(file, "UTF-8");
       fileReader.onload = () => {
         // @ts-ignore
         this.playlist = (JSON.parse(fileReader.result.toString()));
+        this.arr = this.playlist.cliplist;
+
+        for(let i = 0; i < this.arr.length; i++){
+          for(let j = 0; j < this.notAssignable.length; j++){
+            if(this.arr[i].split('.')[0].indexOf(this.notAssignable[j]) > -1){
+              this.nameCorrect[i] = "Name CORRUPTED and CORRECTED!";
+              this.arr[i] = this.replaceUmlaute(this.arr[i]);
+            }
+          }
+        }
       }
       let el = document.getElementById("items");
       // @ts-ignore
@@ -39,14 +54,28 @@ export class JsonModifierComponent implements OnInit {
       this.sortable = sort;
       // save initial order
       let initialOrder = this.sortable.toArray();
-
     }
   }
 
-  onClick(){
-    // @ts-ignore
-    this.sortable?.toArray();
-    console.log(this.sortable?.toArray());
+  replaceUmlaute(str: string) {
+    let value = str.replace(/ä/g, 'ae');
+    value = value.replace(/ö/g, 'oe');
+    value = value.replace(/Ö/g, 'oe');
+    value = value.replace(/Ü/g, 'oe');
+    value = value.replace(/Ä/g, 'oe');
+    value = value.replace(/ü/g, 'ue');
+    value = value.replace(/ß/g, 'ss');
+    value = value.replace(/ /g, '-');
+    value = value.replace(/,/g, '');
+    value = value.replace(/\(/g, '');
+    value = value.replace(/\)/g, '');
+    return value;
+  }
+
+  onSave(){
+    document.getElementsByName("clips").forEach((element, index) => {
+      this.playlist.cliplist[index] = element.innerHTML;
+    })
   }
 
   onExport(){
